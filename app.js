85b6b27ba890c44f522db971133be671230a15f1
Vinicius Ledesma <vinicius.ledesma@totvs.com.br>
2020-07-05 01:55:33 -0300

    Tirando a barra final do cors.

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://alluring-wind-cave-86820.herokuapp.com/',
+    origin: 'https://alluring-wind-cave-86820.herokuapp.com',
   })
 );
 
